8623c9362aba94174f27872ac354d904616d8619
Favour <toluwaniokunowo@gmail.com>
2022-04-11 15:25:40 +0100

    add auth route & logic

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:6',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status_code' => 401,
+                'message' => 'Error Validation',
+                'data' => [
+                    $validator->errors()
+                ],
+            ]);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::query()->create($input);
+        $token = $user->createToken('authtoken')->plainTextToken;
+
+        return response()->json([
+            'status_code' => 200,
+            'message' => 'User Created Successfully',
+            'data' => [
+                'name' => $user->name,
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ],
+        ]);
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        try {
+            $credentials = $request->validate([
+                'email' => 'email|required',
+                'password' => 'required',
+            ]);
+
+            if (!auth()->attempt($credentials)) {
+                return response()->json([
+                    'status_code' => 401,
+                    'message' => 'Unauthorized',
+                ]);
+            }
+
+            $user = auth()->user();
+            $token = $user?->createToken('authtoken')->plainTextToken;
+
+            return response()->json([
+                'status_code' => 200,
+                'message' => 'Authorized',
+                'data' => [
+                    'name' => $user?->name,
+                    'access_token' => $token,
+                    'token_type' => 'Bearer',
+                ],
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'An Error Occurred. Try Again.' . $e
+            ]);
+        }
+    }
+}
