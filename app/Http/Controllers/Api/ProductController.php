f56555ecf456f50f3c96b77dd1344205133040df
Favour <toluwaniokunowo@gmail.com>
2022-04-11 17:56:57 +0100

    complete recommended & popular function

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
@@ -22,7 +23,7 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \App\Http\Requests\StoreProductRequest  $request
+     * @param \App\Http\Requests\StoreProductRequest $request
      * @return \Illuminate\Http\Response
      */
     public function store(StoreProductRequest $request)
@@ -33,7 +34,7 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param \App\Models\Product $product
      * @return \Illuminate\Http\Response
      */
     public function show(Product $product)
@@ -44,8 +45,8 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \App\Http\Requests\UpdateProductRequest  $request
-     * @param  \App\Models\Product  $product
+     * @param \App\Http\Requests\UpdateProductRequest $request
+     * @param \App\Models\Product $product
      * @return \Illuminate\Http\Response
      */
     public function update(UpdateProductRequest $request, Product $product)
@@ -56,11 +57,29 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Product  $product
+     * @param \App\Models\Product $product
      * @return \Illuminate\Http\Response
      */
     public function destroy(Product $product)
     {
         //
+    }
+
+    public function popular(): JsonResponse
+    {
+        $products = Product::query()->latest()->take(5)->get();
+
+        return response()->json([
+            'data' => $products
+        ]);
+    }
+
+    public function recommended(): JsonResponse
+    {
+        $products = Product::query()->inRandomOrder()->take(5)->get();
+
+        return response()->json([
+            'data' => $products
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,11 +23,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-
-//Route::middleware('auth:sanctum')->group(function () {
-//    Route::resource('blogs', BlogController::class);
-//});
-
-Route::group(['middleware' => 'auth:sanctum', 'prefix' => '/api/v1/'], function() {
-
+Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'v1/'], function() {
+    Route::get('products/popular', [ProductController::class, 'popular']);
+    Route::get('products/recommended', [ProductController::class, 'recommended']);
+    Route::resource('products', ProductController::class);
 });
