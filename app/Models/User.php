0a05c00a3cb3cdb05562f8463e6e88c0a7a5731b
Favour <toluwaniokunowo@gmail.com>
2022-04-15 18:48:08 +0100

    add fillable properties

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -18,9 +17,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'f_name',
+        'phone',
         'email',
         'password',
+        'status',
+        'order_count'
     ];
 
     /**
